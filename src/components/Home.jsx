37845691a5dbb1edff2b77fc258daa750c3cf3d7
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-17 14:26:53 -0400

    sync up styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,85 +1,104 @@
-import React, { useState, useEffect } from 'react'
-<<<<<<< HEAD
-import Edit from './Edit'
-import axios from 'axios'
-import Add from './Add'
-import "../styles/Datas.css"
+import React, { useState, useEffect } from "react";
+import Edit from "./Edit";
+import axios from "axios";
+import Add from "./Add";
+import "../styles/Datas.css";
 import "../styles/home.css";
-const Home = () => {
- 
-    let [castings, setCastings] = useState([])
 
-    const getCastings = () => {
-     axios
-       .get('https://bookcast-server.herokuapp.com/api/castings/')
-       .then(
-         (response) => setCastings(response.data),
-         (err) => console.error(err)
-       )
-       .catch((error) => console.error(error))
-    }
+// Home and Castings are currently the same - update later
 
-    const handleCreate = (addCasting) => {
+const Home = (props) => {
+  let [castings, setCastings] = useState([]);
+
+  const getCastings = () => {
     axios
-      .post('https://bookcast-server.herokuapp.com/api/castings/', addCasting)
-      .then((response) => {
-        console.log(response)
-        getCastings()
+      .get("https://bookcast-server.herokuapp.com/api/castings/")
+      .then(
+        (response) => setCastings(response.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error(error));
+  };
+
+  const handleCreate = (addCasting) => {
+    axios
+      .post("https://bookcast-server.herokuapp.com/api/castings/", addCasting, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token " + props.tokenData.token,
+        },
       })
-  }
+      .then((response) => {
+        console.log(response);
+        getCastings();
+      });
+  };
   const handleDelete = (event) => {
     axios
-      .delete('https://bookcast-server.herokuapp.com/api/castings/' + event.target.value)
+      .delete(
+        "https://bookcast-server.herokuapp.com/api/castings/" +
+          event.target.value,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + props.tokenData.token,
+          },
+        }
+      )
       .then((response) => {
-        getCastings()
-      })
-  }
+        getCastings();
+      });
+  };
   const handleUpdate = (editCasting) => {
-    console.log(editCasting)
+    console.log(editCasting);
     axios
-      .put('https://bookcast-server.herokuapp.com/api/castings/' + editCasting.id + '/', editCasting)
+      .put(
+        "https://bookcast-server.herokuapp.com/api/castings/" +
+          editCasting.id +
+          "/",
+        editCasting,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + props.tokenData.token,
+          },
+        }
+      )
       .then((response) => {
-        getCastings()
-      })
-    }
+        getCastings();
+      });
+  };
 
-    useEffect(() => {
-        getCastings()
-       }, [])
+  useEffect(() => {
+    getCastings();
+  }, []);
 
-    return (
-        <>
-        <Add handleCreate={handleCreate} />
-    <div className="castings">
-      {castings.map((casting, index) => {
-        return (
-          <div className="casting" key={casting.id + index}>
-              <h4><span>Date:</span> {casting.created}</h4>
-              <h4><span>Name:</span> {casting.creator}</h4>
-            <h1 className="title">{casting.source_name}</h1>
-            <img src = {casting.source_image_url}></img>
-            <h5><span>Description:</span> {casting.description}</h5>
-            <Edit handleUpdate={handleUpdate} id={casting.id} />
-            <button className="btn" onClick={handleDelete} value={casting.id}>X</button>
-          </div>
-        )
-      })}
-     </div>
-
-
-        
-=======
-
-const Home = () => {
-    return (
-        <>
-        <br/> 
-        <br/>
-        <h1>So.. this is our main page</h1>
-        <h1>What should we put in this page...?</h1>
->>>>>>> 72c0e57fe62e7ece12595bb4bb15724cf8e4ff3c
-        </>
-    )
-}
+  return (
+    <>
+      <Add
+        handleCreate={handleCreate}
+        // userData={props.userData}
+        // tokenData={props.tokenData}
+        // errorData={props.errorData}
+      />
+      <div className="castings">
+        {castings.map((casting, index) => {
+          return (
+            <div className="casting" key={casting.id + index}>
+              <h4>Date: {casting.created}</h4>
+              <h4>{casting.source_name}</h4>
+              <img src={casting.source_image_url}></img>
+              <h5>Description: {casting.description}</h5>
+              <Edit handleUpdate={handleUpdate} id={casting.id} />
+              <button onClick={handleDelete} value={casting.id}>
+                X
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Home;
