f72934b2d9cda271f9255671cd0b6197272a8875
Jihee Kim <jiheekim93@gmail.com>
2022-03-17 17:14:44 -0400

    search bar added

--- src/components/Casting.jsx
+++ src/components/Casting.jsx
@@ -1,110 +1,83 @@
-import React, { useState, useEffect } from "react";
-import Edit from "./Edit";
-import axios from "axios";
-import Add from "./Add";
+import React, { useState, useEffect } from 'react'
+import Edit from './Edit'
+import axios from 'axios'
+import Add from './Add'
+import "../styles/Datas.css"
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/Datas.css";
 
-const Casting = (props) => {
-  let [castings, setCastings] = useState([]);
+const Casting = () => {
 
-  let navigate = useNavigate();
+    let [castings, setCastings] = useState([])
 
-  const getCastings = () => {
+    let navigate = useNavigate();
+
+    const getCastings = () => {
+     axios
+       .get('https://bookcast-server.herokuapp.com/api/castings/')
+       .then(
+         (response) => setCastings(response.data),
+         (err) => console.error(err)
+       )
+       .catch((error) => console.error(error))
+    }
+
+    const handleCreate = (addCasting) => {
     axios
-      .get("https://bookcast-server.herokuapp.com/api/castings/")
-      .then(
-        (response) => setCastings(response.data),
-        (err) => console.error(err)
-      )
-      .catch((error) => console.error(error));
-  };
-
-  const handleCreate = (addCasting) => {
-    axios
-      .post("https://bookcast-server.herokuapp.com/api/castings/", addCasting, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Token " + props.tokenData.token,
-        },
-      })
+      .post('https://bookcast-server.herokuapp.com/api/castings/', addCasting)
       .then((response) => {
-        console.log(response);
-        getCastings();
-      });
-  };
+        console.log(response)
+        getCastings()
+      })
+  }
   const handleDelete = (event) => {
     axios
-      .delete(
-        "https://bookcast-server.herokuapp.com/api/castings/" +
-          event.target.value,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Token " + props.tokenData.token,
-          },
-        }
-      )
+      .delete('https://bookcast-server.herokuapp.com/api/castings/' + event.target.value)
       .then((response) => {
-        getCastings();
-      });
-  };
+        getCastings()
+      })
+  }
   const handleUpdate = (editCasting) => {
-    console.log(editCasting);
+    console.log(editCasting)
     axios
-      .put(
-        "https://bookcast-server.herokuapp.com/api/castings/" +
-          editCasting.id +
-          "/",
-        editCasting,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Token " + props.tokenData.token,
-          },
-        }
-      )
+      .put('https://bookcast-server.herokuapp.com/api/castings/' + editCasting.id + '/', editCasting)
       .then((response) => {
-        getCastings();
-      });
-  };
+        getCastings()
+      })
+    }
 
-  useEffect(() => {
-    getCastings();
-  }, []);
+    useEffect(() => {
+        getCastings()
+       }, [])
 
-  return (
-    <>
-      <Add
-        handleCreate={handleCreate}
-        // userData={props.userData}
-        // tokenData={props.tokenData}
-        // errorData={props.errorData}
-      />
-      <div className="castings">
-        {castings.map((casting, index) => {
-          return (
-            <div
+    return (
+        <>
+        <Add handleCreate={handleCreate} />
+    <div className="castings">
+      {castings.map((casting, index) => {
+        return (
+          <div
               className="casting"
               key={casting.id + index}
               onClick={() => {
                 navigate("/cast/" + casting.id);
               }}
             >
-              <h4>Date: {casting.created}</h4>
-              <h4>{casting.source_name}</h4>
-              <img src={casting.source_image_url}></img>
-              <h5>Description: {casting.description}</h5>
+            <h4>Date: {casting.created}</h4>
+            <h4>{casting.source_name}</h4>
+            <img src = {casting.source_image_url}></img>
+            <h5>Description: {casting.description}</h5>
               <Edit handleUpdate={handleUpdate} id={casting.id} />
-              <button onClick={handleDelete} value={casting.id}>
-                X
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-};
+            <button onClick={handleDelete} value={casting.id}>X</button>
+          </div>
+        )
+      })}
+     </div>
+
+
+        
+        </>
+  
+    )
+}
 
 export default Casting;
--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,10 +1,74 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import "../styles/Datas.css"
+import Add from './Add'
 
-const Discover = () => {
+
+
+const Discover = (props) => {
+    let [datas, setDatas] = useState([])
+    let [filter, setFilter] = useState('')
+    
+    const getDatas = () => {
+        axios
+        .get('https://bookcast-server.herokuapp.com/api/castings/')
+        .then(
+          (response) => setDatas(response.data),
+          (err) => console.error(err)
+        )
+        .catch((error) => console.error(error))
+     }
+
+
+      const handleDelete = (event) => {
+        axios
+          .delete('https://bookcast-server.herokuapp.com/api/castings/' + event.target.value, 
+          {
+              headers: {
+              "Content-Type": "application/json",
+              Authorization: "Token " + props.tokenData.token,
+            },
+          }
+          )
+          .then((response) => {
+            setDatas()
+          })
+      }
+
+    
+      useEffect(() => {
+
+        getDatas()
+       }, [])
+
+
+
+
     return (
         <>
         <h1>discover</h1>
+        <div className = 'searchDiv'>
+
+<input className = 'searchInput' type="text" placeholder="search..." value={filter} onChange={(e) => {e.preventDefault(); setFilter(e.target.value);
+}}
+></input>
+</div>
+        <div className="castings">
+
+      {datas.filter((search) =>
+        search.source_name.toLowerCase().includes(filter.toLowerCase())).map((data, index) => {
+        return (
+          <div className="casting" key={data.id + index}>
+              <h4>Date: {data.created}</h4>
+            <h4>{data.source_name}</h4>
+            <img src = {data.source_image_url}></img>
+            <h5>Description: {data.description}</h5>
+            <button onClick={handleDelete} value={data.id}>X</button>
+          </div>
+        )
+      })}
+     </div>
+
         </>
     )
 }
