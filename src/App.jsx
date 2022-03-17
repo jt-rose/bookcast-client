d1c5087198d95736afd75fc1ecb48c92012ee9ba
Jihee Kim <jiheekim93@gmail.com>
2022-03-17 11:45:01 -0400

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,32 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Discover from "./components/Discover";
-import RecentList from "./components/RecentList";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
-import Home from "./components/Home";
+import Casting from "./components/Casting";
 import "./styles/NavBar.css";
 import Auth from "./components/Auth";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
       <div className="navbar">
-        <img src="../images/bookcastlogo.png" />
-        <Link className="link" to="/">
-          Home
+       <Link className="link" to = '/'> <img className="logo" src="../images/bookcastlogo.png" /> </Link>
+        <Link className="link" to="/casting">
+          Casting
         </Link>
         <Link className="link" to="/discover">
           Discover
-        </Link>
-        <Link className="link" to="/recent-list">
-          Recent List
-        </Link>
-        <Link className="link" to="/profile">
-          Profile
         </Link>
         <Link className="link" to="/login">
           Login
         </Link>
       </div>
-      
+      {/* castings, Discover   login */}
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/casting" element={<Casting />} />
         <Route path="/discover" element={<Discover />} />
-        <Route path="/recent-list" element={<RecentList />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/login" element={<Auth />} />
       </Routes>
     </>
   );
