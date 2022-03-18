79cf59998062156be21319ed4df9ec091e837464
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-18 13:25:52 -0400

    set up castings edit

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Character from "./Character";
 
-const Cast = () => {
+const Cast = (props) => {
   const params = useParams();
   const [castDatas, setCastDatas] = useState(null);
-  const [editName, setEditName] = useState(false);
+  const [edit, setEdit] = useState(false);
   const [name, setName] = useState("");
+  const [image_url, setImage_url] = useState("");
+  const [description, setDescription] = useState("");
 
   let castingVotes = 0;
   if (castDatas && castDatas.votes) {
@@ -15,6 +17,36 @@ const Cast = () => {
       .map((vote) => (vote.like ? 1 : -1))
       .reduce((x, y) => x + y, 0);
   }
+
+  let isCreator = false;
+  if (props.userData.user && castDatas) {
+    isCreator = props.userData.user.id === castDatas.creator.id;
+  }
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    axios
+      .put(
+        "https://bookcast-server.herokuapp.com/api/castings/" +
+          params.castid +
+          "/",
+        {
+          source_name: name,
+          source_image_url: image_url,
+          description: description,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + props.tokenData.token,
+          },
+        }
+      )
+      .then((response) => {
+        setCastDatas(response.data);
+        setEdit(false);
+      })
+      .catch((err) => props.errorData.setError(err));
+  };
 
   useEffect(() => {
     axios
@@ -25,6 +57,8 @@ const Cast = () => {
         console.log(response);
         setCastDatas(response.data);
         setName(response.data.source_name);
+        setImage_url(response.data.source_image_url);
+        setDescription(response.data.description);
       });
   }, []);
 
@@ -34,18 +68,43 @@ const Cast = () => {
     return (
       <>
         <h1>Hi, this is cast id: {params.castid}</h1>
-        {!editName && <h2>{castDatas.source_name}</h2>}
-        {editName && (
-          <div>
-            <input value={name} onChange={(e) => setName(e.target.value)} />
-            <button onClick={() => {}}>Update</button>
-          </div>
-        )}
-        <button onClick={() => setEditName(!editName)}>
-          {!editName ? "Edit" : "Cancel"}
-        </button>
+        <h2>{castDatas.source_name}</h2>
+
         <img src={castDatas.source_image_url}></img>
         <h5>Description: {castDatas.description}</h5>
+
+        <br />
+        {isCreator && (
+          <button onClick={() => setEdit(!edit)}>
+            {!edit ? "Edit Casting Info" : "Cancel"}
+          </button>
+        )}
+        {edit && (
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="name">Description</label>
+            <textarea
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <label htmlFor="image-url">Image URL</label>
+            <input
+              type="text"
+              id="image-url"
+              value={image_url}
+              onChange={(e) => setImage_url(e.target.value)}
+            />
+            <button onClick={handleUpdate}>Update Casting Info</button>
+          </div>
+        )}
+        <br />
         <input placeholder="...share a comment"></input>
         <input type="checkbox" name="like" id="like" />
         <h5>Casting Vote: {castingVotes}</h5>
