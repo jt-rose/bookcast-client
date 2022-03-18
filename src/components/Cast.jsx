c9f2fdc8b5f65fed95a4abf04bc2690d0872bfb4
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-17 23:35:08 -0400

    add character cards

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,68 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import Casting from "./Casting";
+import Character from "./Character";
 
-const Cast = (props) => {
+const Cast = () => {
   const params = useParams();
-  const [castDatas, setCastDatas] = useState([])
-  const [characters, setCharacters] = useState([])
-  const [users, setUsers] = useState([])
-  const [votes, setVotes] = useState([])
-
-
-  
-  useEffect(() => {
-    axios
-      .get(
-        "https://bookcast-server.herokuapp.com/api/castings/" + params.castid
-      )
-      .then((response) => 
-      setCastDatas(response.data));
-  }, []);
-  
-  useEffect(() => {
-    axios
-      .get(
-        "https://bookcast-server.herokuapp.com/api/castings/" + params.castid
-      )
-      .then((response) => 
-      setCharacters(response.data.characters[0]));
-  }, []);
-  
-  useEffect(() => {
-    axios
-      .get(
-        "https://bookcast-server.herokuapp.com/api/castings/" + params.castid
-      )
-      .then((response) => 
-      setUsers(response.data.creator));
-  }, []);
+  const [castDatas, setCastDatas] = useState([]);
 
   useEffect(() => {
     axios
       .get(
         "https://bookcast-server.herokuapp.com/api/castings/" + params.castid
       )
-      .then((response) => 
-      setVotes(response.data.characters[0].votes_and_comments[0]));
+      .then((response) => {
+        console.log(response);
+        setCastDatas(response.data);
+      });
   }, []);
-  
 
-  return (
-    <>
-    
-  <h1>Hi, this is cast id: {params.castid}</h1>
-  <h2>{castDatas.source_name}</h2>
-  <img src = {castDatas.source_image_url}></img>
-  <h5>Description: {castDatas.description}</h5>
-  <h5>Actor: {characters.actor}</h5>
-  <h5>Role Name: {characters.name}</h5>
-  <h5>ID: {users.username}</h5>
-  <h5>Comments: {votes.comment}</h5>
-  {/* <button onClick={handleDelete} value={castDatas.id}>X</button> */}
-    </>
-  )
+  if (!castDatas) {
+    return <p>...loading</p>;
+  } else {
+    return (
+      <>
+        <h1>Hi, this is cast id: {params.castid}</h1>
+        <h2>{castDatas.source_name}</h2>
+        <img src={castDatas.source_image_url}></img>
+        <h5>Description: {castDatas.description}</h5>
+        {castDatas.characters.map((char) => (
+          <Character character={char} />
+        ))}
+
+        {/* <button onClick={handleDelete} value={castDatas.id}>X</button> */}
+      </>
+    );
+  }
 };
 
 export default Cast;
