701e48f9c20693c14b4b4208aac60eaea292bd6b
Scarlet Puma <scarlt.puma@gmail.com>
2022-03-18 10:16:53 -0400

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,9 @@ import "../styles/footer.css";
 
 const Footer = () => {
     return(
-    <div className="footer">
-        <p>created by <a href="/">jihee</a>, <a href="/">jeff</a>, & <a href="/">scarlet</a> </p>
-    </div>
-    )}
+        <div className="footer">
+            <p>created by <a href="/">jihee</a>, <a href="/">jeff</a>, & <a href="/">scarlet</a> </p>
+        </div>
+    )
+}
 export default Footer
