b1d79f5d09d55414f47c749deae01783cd7cb054
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-18 15:44:54 -0400

    remove unused state

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Character from "./Character";
 import "../styles/cast.css";
-import { FaHeart, FaHeartBroken } from 'react-icons/fa';
+import { FaHeart, FaHeartBroken } from "react-icons/fa";
 import AddCharacter from "./AddCharacter";
 
 const Cast = (props) => {
@@ -99,83 +99,80 @@ const Cast = (props) => {
       <>
         <h1>Hi, this is cast id: {params.castid}</h1>
         <div className="mediadiv">
-        <div className="castinfo">
-        <h2 className="title">{castDatas.source_name}</h2>
-
-        <img src={castDatas.source_image_url}></img>
-        <h5>Description: {castDatas.description}</h5>
-
-        <br />
-        {isCreator && (
-          <button onClick={() => setEdit(!edit)}>
-            {!edit ? "Edit Casting Info" : "Cancel"}
-          </button>
-        )}
-        {edit && (
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label htmlFor="name">Description</label>
-            <textarea
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <label htmlFor="image-url">Image URL</label>
-            <input
-              type="text"
-              id="image-url"
-              value={image_url}
-              onChange={(e) => setImage_url(e.target.value)}
-            />
-            <button onClick={handleUpdate}>Update Casting Info</button>
+          <div className="castinfo">
+            <h2 className="title">{castDatas.source_name}</h2>
+            <img src={castDatas.source_image_url}></img>
+            <h5>Description: {castDatas.description}</h5>
+            <br />
+            {isCreator && (
+              <button onClick={() => setEdit(!edit)}>
+                {!edit ? "Edit Casting Info" : "Cancel"}
+              </button>
+            )}
+            {edit && (
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <label htmlFor="name">Description</label>
+                <textarea
+                  id="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                <label htmlFor="image-url">Image URL</label>
+                <input
+                  type="text"
+                  id="image-url"
+                  value={image_url}
+                  onChange={(e) => setImage_url(e.target.value)}
+                />
+                <button onClick={handleUpdate}>Update Casting Info</button>
+              </div>
+            )}
+            <img src={castDatas.source_image_url}></img>
+            <br />
+            <FaHeart /> <FaHeartBroken />
+            <h5>
+              <span>Description:</span> {castDatas.description}
+            </h5>
           </div>
-        )}
-        <button onClick={() => setEditName(!editName)}>
-          {!editName ? "Edit" : "Cancel"}
-        </button>
-        
-          <img src={castDatas.source_image_url}></img><br />
-          <FaHeart /> <FaHeartBroken />
-          <h5><span>Description:</span> {castDatas.description}</h5>
+          <br />
+          <input placeholder="...share a comment"></input>
+          <input type="checkbox" name="like" id="like" />
+          <h5>Casting Vote: {castingVotes}</h5>
+          <h5>Casting Comments:</h5>
+          {castDatas &&
+            castDatas.comments.map((comment) => (
+              <p key={"comment-" + comment.id}>
+                {comment.user.username}: {comment.comment}
+              </p>
+            ))}
         </div>
-        <br />
-        <input placeholder="...share a comment"></input>
-        <input type="checkbox" name="like" id="like" />
-        <h5>Casting Vote: {castingVotes}</h5>
-        <h5>Casting Comments:</h5>
-        {castDatas &&
-          castDatas.comments.map((comment) => (
-            <p key={"comment-" + comment.id}>
-              {comment.user.username}: {comment.comment}
-            </p>
-          ))}
-          </div>
-          <div className="characterdiv">
-        {castDatas &&
-          castDatas.characters.map((char) => (
-            <Character
-              key={"char" + char.id}
-              character={char}
+        <div className="characterdiv">
+          {castDatas &&
+            castDatas.characters.map((char) => (
+              <Character
+                key={"char" + char.id}
+                character={char}
+                tokenData={props.tokenData}
+                getCasting={getCasting}
+                errorData={props.errorData}
+                isCreator={isCreator}
+              />
+            ))}
+          {isCreator && (
+            <AddCharacter
               tokenData={props.tokenData}
-              getCasting={getCasting}
               errorData={props.errorData}
-              isCreator={isCreator}
+              getCasting={getCasting}
+              castingId={castDatas.id}
             />
-          ))}
-        {isCreator && (
-          <AddCharacter
-            tokenData={props.tokenData}
-            errorData={props.errorData}
-            getCasting={getCasting}
-            castingId={castDatas.id}
-          />
-        )}
+          )}
         </div>
 
         {!willDelete && (
@@ -189,11 +186,10 @@ const Cast = (props) => {
             </p>
             <button onClick={handleDelete}>Yes, delete</button>
             <button onClick={() => setWillDelete(false)}>no thanks</button>
-            </>
-           
+          </>
         )}
-        </>
-    )
+      </>
+    );
   }
-}
+};
 export default Cast;
